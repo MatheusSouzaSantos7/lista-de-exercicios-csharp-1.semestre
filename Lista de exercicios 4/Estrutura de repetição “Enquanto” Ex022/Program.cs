391439dfca51efd3abfd3894f8b47787e51264e5
Matheus Souza <matheus.sax7@gmail.com>
2023-05-03 14:44:54 -0300

    exercícios 22 e 23 feitos.

---
+++ Lista de exercicios 4/Estrutura de repetição “Enquanto” Ex022/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_de_repetição__Enquanto__Ex022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Objetivo: Calcular o fatorial de um número. O valor não pode ser negativo. Perguntar se quer fazer um novo cálculo, S ou N */
+            int n, a;
+            string b;
+            while (true)
+            {
+                Console.Write("Digite um número: ");
+                while (true)
+                {
+                    n = int.Parse(Console.ReadLine());
+                    if (n < 0)
+                    {
+                        Console.Write("NÚMERO NEGATIVO! Digite outro número: ");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                a = n-1;
+                Console.Write("O fatorial de {0} é ", n);
+                if (a == -1)
+                {
+                    n =  1;
+                }
+                while (a >= 1)
+                {
+                    n = n * a;
+                    a--;
+                }
+                Console.WriteLine(n);
+                while (true)
+                {
+                    Console.WriteLine("Deseja calcular um novo número? S ou N:");
+                    b = Console.ReadLine();
+                    if (b == "N")
+                    {
+                        break;
+                    }
+                    else if (b == "S")
+                    {
+                        Console.WriteLine("Reiniciando...");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Comando não aceito! escolha S ou N");
+                    }
+                }
+                if (b == "N")
+                {
+                    break;
+                }
+            }
+            Console.Write("Aperte qualquer tecla para sair");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lista de exercicios 4/Estrutura de repetição “Enquanto” Ex023/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_de_repetição__Enquanto__Ex023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Objetivo: . Em um jogo de adivinhação, o jogador deve descobrir um número entre 1 e 10
+               usando até três tentativas. A cada tentativa o jogador é informado se o número foi
+               descoberto, ou se ele é maior ou menor do que a tentativa. Se após três tentativas o
+               jogador não descobrir o número, então ele perde o jogo. Faça um algoritmo que
+               implemente esse jogo */
+
+            int n, r, ct;
+            Console.WriteLine("Descubra o valor. Você tem 3 tentativas.");
+            Console.Write("Digite um valor entre 0 e 10: ");
+            for (ct = 2; ct >= 0; ct--) // A partir do momento em que o usuário vai digitar um valor, ele já gastou uma tentativa. Por isso que começa com 2 tentativas.
+            {
+                n = 5; // O valor que deve ser acertado é 5.
+                r = int.Parse(Console.ReadLine()); // r é o valor que o usuário informou.
+                Console.WriteLine();
+                if (r > 10)
+                {
+                    Console.Write("Valor maior que 10. Escreva outro valor: ");
+                    ct++;
+                }
+                else if (r < 0)
+                {
+                    Console.Write("Valor menor que 0, peça outro valor: ");
+                    ct++;
+                }
+                else if (r > n)
+                {
+                    Console.WriteLine("O número digitado é maior que o número certo");
+                    Console.Write("Você tem {0} tentativas, digite outro valor: ", ct);
+                }
+                else if (r < n)
+                {
+                    Console.WriteLine("O número digitado é menor que o número certo");
+                    Console.Write("Você tem {0} tentativas, digite outro valor: ", ct);
+                }
+                if (r == n)
+                {
+                    Console.WriteLine("Você acertou! Se errasse, você teria mais {0} tentativas. Aperte qualquer botão para sair", ct);
+                    Console.ReadKey();
+                    ct = -1;
+                }
+                if (ct == 0)
+                {
+                    Console.WriteLine("Você perdeu! Aperte qualquer tecla para sair");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
